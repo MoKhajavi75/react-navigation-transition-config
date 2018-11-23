728711679e5c035245581d35aa940fbfccd6c092
MohamadKh75 [windows] <khajavi_mohamad@yahoo.com>
2018-11-23 22:00:21 +0330

    Add fadeIn

---
+++ src/fadeIn/index.js
@@ -0,0 +1,25 @@
+import { Animated, Easing } from "react-native";
+
+function fadeIn(duration = 300) {
+  return {
+    transitionSpec: {
+      duration,
+      easing: Easing.out(Easing.poly(4)),
+      timing: Animated.timing,
+      useNativeDriver: true
+    },
+
+    screenInterpolator: ({ position, scene }) => {
+      const { index } = scene;
+
+      const opacity = position.interpolate({
+        inputRange: [index - 1, index],
+        outputRange: [0, 1]
+      });
+
+      return { opacity };
+    }
+  };
+}
+
+export { fadeIn };
