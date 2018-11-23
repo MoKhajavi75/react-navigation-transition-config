da42cce3cab0b9a287192f5a19406fa12087bddf
MohamadKh75 [windows] <khajavi_mohamad@yahoo.com>
2018-11-23 21:59:07 +0330

    Add fromTop

---
+++ src/fromTop/index.js
@@ -0,0 +1,31 @@
+import { Animated, Easing } from "react-native";
+
+function fromTop(duration = 300) {
+  return {
+    transitionSpec: {
+      duration,
+      easing: Easing.out(Easing.poly(4)),
+      timing: Animated.timing,
+      useNativeDriver: true
+    },
+
+    screenInterpolator: ({ layout, position, scene }) => {
+      const { index } = scene;
+      const { initHeight } = layout;
+
+      const translateY = position.interpolate({
+        inputRange: [index - 1, index, index + 1],
+        outputRange: [-initHeight, 0, 0]
+      });
+
+      const opacity = position.interpolate({
+        inputRange: [index - 1, index - 0.99, index],
+        outputRange: [0, 1, 1]
+      });
+
+      return { opacity, transform: [{ translateY }] };
+    }
+  };
+}
+
+export { fromTop };
