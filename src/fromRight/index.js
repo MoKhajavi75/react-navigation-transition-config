a162a13d9e44935f4c0a20c40a2c04edc7b4cf59
MohamadKh75 [windows] <khajavi_mohamad@yahoo.com>
2018-11-23 21:58:56 +0330

    Add fromRight

---
+++ src/fromRight/index.js
@@ -0,0 +1,31 @@
+import { Animated, Easing } from "react-native";
+
+function fromRight(duration = 300) {
+  return {
+    transitionSpec: {
+      duration,
+      easing: Easing.out(Easing.poly(4)),
+      timing: Animated.timing,
+      useNativeDriver: true
+    },
+
+    screenInterpolator: ({ layout, position, scene }) => {
+      const { index } = scene;
+      const { initWidth } = layout;
+
+      const translateX = position.interpolate({
+        inputRange: [index - 1, index, index + 1],
+        outputRange: [+initWidth, 0, 0]
+      });
+
+      const opacity = position.interpolate({
+        inputRange: [index - 1, index - 0.99, index],
+        outputRange: [0, 1, 1]
+      });
+
+      return { opacity, transform: [{ translateX }] };
+    }
+  };
+}
+
+export { fromRight };
